257cb27ea4e17646762206230bc82baf4495a151
Andrew Nolan <anolan@lanl.gov>
2025-07-31 19:31:07 -0500

    Fix spack import for v1.0.0 API

--- repos/spack_repo/e3sm/packages/trilinos_for_albany/package.py
+++ repos/spack_repo/e3sm/packages/trilinos_for_albany/package.py
@@ -9,7 +9,7 @@ import sys
 from spack.operating_systems.mac_os import macos_version
 from spack_repo.builtin.build_systems.cmake import CMakePackage
 
-from spack import *
+from spack.package import *
 
 # Trilinos is complicated to build, as an inspiration a couple of links to
 # other repositories which build it:
